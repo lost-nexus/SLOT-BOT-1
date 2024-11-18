14a372a3cec203e69598f246fd02c5ae2751f570
Nexus.!!ゞ <rikeshsamanta@gmail.com>
2024-11-18 21:53:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -53,3 +53,15 @@ async function runDailyTask() {
     }
     // Add your task logic here
 }
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
