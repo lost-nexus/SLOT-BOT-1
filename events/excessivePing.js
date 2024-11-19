dbe3d7462f8eb544cc4e5f2f3da255bb25dae9eb
Nexus.!!ゞ <rikeshsamanta@gmail.com>
2024-11-19 19:27:58 +0530

    Update excessivePing.js

--- events/excessivePing.js
+++ events/excessivePing.js
@@ -33,17 +33,20 @@ module.exports = {
             var count = await db.get(`count.${message.channel.id}`)
             if (!count) {
                 await db.set(`count.${message.channel.id}`, 1)
-                message.channel.send({embeds: [{description: `You have used 1/2 pings`, color: 0xff0000}]})
+                message.channel.send({embeds: [{description: `You have used 1/3 pings`, color: 0xff0000}]})
             } else if (count === 1) {
                 await db.set(`count.${message.channel.id}`, 2)
-                message.channel.send({embeds: [{description: `You have used 2/2 pings`, color: 0xff0000}]})
-            } else if (count === 2) {
+                message.channel.send({embeds: [{description: `You have used 2/3 pings`, color: 0xff0000}]})
+            }  else if (count === 2) {
+                await db.set(`count.${message.channel.id}`, 3)
+                message.channel.send({embeds: [{description: `You have used 3/3 pings`, color: 0xff0000}]})
+} else if (count === 3) {
                 await message.channel.permissionOverwrites.edit(message.author.id, { SendMessages: false });
     
                 client.giveawaysManager
                     .pause(onServer[0].messageId)
                     .then(() => {
-                        message.reply({embeds: [{title: `Slot has been paused`, description: `Reason: Use of More than 2 pings`, color: 0xff0000}]});
+                        message.reply({embeds: [{title: `Slot has been paused`, description: `Reason: Use of More than 3 pings`, color: 0xff0000}]});
                     })
                     .catch((err) => {
                         message.reply({ephemeral: true, content: `An error has occurred, please check and try again.\n\`${err}\``});
